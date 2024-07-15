94c3ad8a4cdc0f0851cc70b36318cb0146782f5a
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-07-15 13:55:22 +0600

    add login or proteact all page

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,145 +1,120 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { useAppDispatch } from "../../redux/hooks";
-import { useLoginMutation } from "../../redux/features/auth/authApi";
 import { toast } from "sonner";
-import image from '../../assets/image/labOneLogo.png'
+import image from "../../assets/image/labOneLogo.png";
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Col, Form, Input, Row } from "antd";
-
-type ErrorResponse = {
-  message: string;
-  status_code: string;
-  reason: string;
-};
+import { useLoginMutation } from "../../redux/features/auth/authApi";
+import { verifyToken } from "../../utils/verifyToken";
+import { setUser } from "../../redux/features/auth/authSlice";
 
 const Login = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [form] = Form.useForm();
+  const [isLoading, setIsLoading] = useState(false);
+  const [form] = Form.useForm();
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const [login] = useLoginMutation();
 
-  const [toggle, setToggle] = useState(false);
+  const onSubmit = async (data: any) => {
+    setIsLoading(true);
+    const toastId = toast.loading("Admin login...");
 
-  // const defaultValues = {
-  //   email: "shakilahamedctg123@gmail.com",
-  //   password: "fasterboyshakil",
-  // };
+    try {
+      const userInfo = await login(data).unwrap();
+      const user = verifyToken(userInfo?.data?.accessToken);
 
-//   const onSubmit: SubmitErrorHandler<FieldValues> = async (data) => {
-//     const tostId = toast.loading("Logging In");
-//     try {
-//       const userInfo = await login(data).unwrap();
+      dispatch(setUser({ user: user, token: userInfo?.data?.accessToken }));
 
-//       const user = verifyToken(userInfo.token);
+      if (user?.role === "admin") {
+        toast.success("Logged In successful!", { id: toastId, duration: 2000 });
+        localStorage.setItem("accessToken", userInfo?.data?.accessToken);
+        navigate("/");
+        setIsLoading(false);
+      } else {
+        toast.error("Your are Unauthorized!", {
+          id: toastId,
+          duration: 2000,
+        });
+        setIsLoading(false);
+      }
+    } catch (error: any) {
+      setIsLoading(false);
 
-//       dispatch(setUser({ user: user, token: userInfo.token }));
-
-//       if ((user as TUser)?.isAdmin) {
-//         toast.success("Logged In successful!", { id: tostId, duration: 2000 });
-//       } else {
-//         toast.error("Your are Unauthorized!", {
-//           id: tostId,
-//           duration: 2000,
-//         });
-//       }
-
-//       navigate("/");
-//     } catch (error: any) {
-//       if (error.data) {
-//         const { reason } = error.data as ErrorResponse;
-//         toast.error(reason, { id: tostId });
-//       }
-//     }
-//   };
-const onSubmit = async (data: any) => {
-    const toastId = toast.loading("Adding new doctor...");
-
-   
-    console.log( data );
-
-
-
-    // try {
-    //   setIsLoading(true);
-    //   toast.success("Successfully added the doctor", { id: toastId });
-    // } catch (error: any) {
-    //   toast.error(error.message, { id: toastId });
-    // } finally {
-    //   setIsLoading(false);
-    // }
+      if (error?.data?.message) {
+        toast.error(error?.data?.message, { id: toastId });
+      }
+    }
   };
 
   return (
     <div className="max-w-[450px] mx-auto">
       <div className="bg-white min-h-[470px] rounded-xl px-6 ">
         <div className="w-full text-center pb-8">
-          <img
-            className="inline-block mt-10"
-            src={image}
-            alt="img"
-          />
+          <img className="inline-block mt-10" src={image} alt="img" />
           <h1 className="text-2xl font-bold pt-4 pb-2">Let’s Sign You In</h1>
           <p>Welcome back, you’ve been missed!</p>
         </div>
 
         <Row>
-        <Col span={24}>
-          <Form
-            form={form}
-            onFinish={onSubmit}
-            requiredMark={false}
-            layout="vertical"
-          >
-            <Row gutter={16} className="flex items-center flex-col w-full">
-              <Col span={24}  className="!w-full">
-                <Form.Item
-                  label="Enter your email"
-                  name="email"
-                 
-                  tooltip="Here you have to input  your email."
-                  rules={[{ required: true, message: "Email is required" }]}
-                >
-                  <Input
-                    type="email"
-                    placeholder="Enter type your email..."
-                    className="h-10 border border-[#C4CAD4] !rounded-lg"
-                  />
-                </Form.Item>
-              </Col>
-              <Col span={24} className="!w-full">
-                <Form.Item
-                  label="Your Password"
-                  name="password"
-                  tooltip="Here you have to input your Password."
-                  rules={[{ required: true, message: "Password is Required!" }]}
-                >
-                  <Input.Password
-                    type="number"
-                    placeholder="Enter type your password..."
-                    className="h-10 border border-[#C4CAD4] !rounded-lg"
-                  />
-                </Form.Item>
-              </Col>
-            </Row>
+          <Col span={24}>
+            <Form
+              form={form}
+              onFinish={onSubmit}
+              requiredMark={false}
+              layout="vertical"
+            >
+              <Row gutter={16} className="flex items-center flex-col w-full">
+                <Col span={24} className="!w-full">
+                  <Form.Item
+                    label="Enter your email"
+                    name="email"
+                    initialValue={"jahidmorol2@gmail.com"}
+                    tooltip="Here you have to input  your email."
+                    rules={[{ required: true, message: "Email is required" }]}
+                  >
+                    <Input
+                      type="email"
+                      placeholder="Enter type your email..."
+                      className="h-10 border border-[#C4CAD4] !rounded-lg"
+                    />
+                  </Form.Item>
+                </Col>
+                <Col span={24} className="!w-full">
+                  <Form.Item
+                    label="Your Password"
+                    name="password"
+                    initialValue={"Jahid00@11"}
+                    tooltip="Here you have to input your Password."
+                    rules={[
+                      { required: true, message: "Password is Required!" },
+                    ]}
+                  >
+                    <Input.Password
+                      type="password"
+                      placeholder="Enter type your password..."
+                      className="h-10 border border-[#C4CAD4] !rounded-lg"
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
 
-            <Row>
-              <div className="flex items-center justify-center w-full">
-                <button
-                  className="cursor-pointer w-full hover:bg-gray-950 py-1.5 bg-primary font-medium  text-white rounded-lg"
-                  type="submit"
-                  disabled={isLoading ? true : false}
-                >
-                  {isLoading ? "Loading..." : "Login"}
-                </button>
-              </div>
-            </Row>
-          </Form>
-        </Col>
-      </Row>
+              <Row>
+                <div className="flex items-center justify-center w-full">
+                  <button
+                    className="cursor-pointer w-full hover:bg-gray-950 py-1.5 bg-primary font-medium  text-white rounded-lg"
+                    type="submit"
+                    disabled={isLoading ? true : false}
+                  >
+                    {isLoading ? "Loading..." : "Login"}
+                  </button>
+                </div>
+              </Row>
+            </Form>
+          </Col>
+        </Row>
       </div>
     </div>
   );
